f567f03cd278c457623b8cf42e150638478d8e25
Aaron Jackson <aaron.jackson@gmail.com>
2012-04-08 00:10:47 -0600

    Moved timespan formatting to separate object and CustomBinding

--- design/html/js/script.js
+++ design/html/js/script.js
@@ -10,9 +10,52 @@ ko.bindingHandlers.dateString = {
     }
 }
 
+ko.bindingHandlers.timespan = {
+    update: function(element, valueAccessor, allBindingsAccessor, viewModel) {
+        var value = valueAccessor(), allBindings = allBindingsAccessor();
+        var valueUnwrapped = ko.utils.unwrapObservable(value);
+		var pattern = allBindings.pattern || 'minutes';
+		var timespan = new Timespan(valueUnwrapped);
+		if (pattern === 'minutes') {
+	        $(element).text(timespan.minutes);
+        } else if (pattern === 'seconds') {
+	        $(element).text(pad(timespan.seconds,2));
+        } else if (pattern === 'hours') {
+        	$(element).text(timespan.hours);
+        }        
+    }
+}
+
+function pad(num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
+
+function Timespan(timespan) {
+    var self = this;
+    self.days=Math.floor(timespan / 86400);
+    self.hours = Math.floor((timespan - (self.days * 86400 ))/3600)
+    self.minutes = Math.floor((timespan - (self.days * 86400 ) - (self.hours * 3600 ))/60)
+    self.seconds = Math.floor((timespan - (self.days* 86400 ) - (self.hours * 3600 ) - (self.minutes*60)))
+}
+
+
+
 var viewModel = new ViewModel();
 viewModel.today(new Date());
 viewModel.totalTime(0);
+
+var task1 = new Task('Email, planning, and time tracking');
+task1.description = 'Task for tracking general administrative time.';
+task1.dueDate = new Date(2012, 3, 5, 8, 30, 0, 0);
+task1.tags.push('Administrative');
+task1.status = new TaskStatus(1, 'New');
+task1.time = new TimeEntry(new Date(2012, 3, 1, 8, 00, 0, 0));
+task1.time.stop = new Date(2012, 3, 1, 8, 30, 0, 0);
+task1.time.note = 'Your eyes can deceive you. Don\'t trust them. Kid, I\'ve flown from one side of this galaxy to the other.'
+viewModel.tasks.push(task1);
+
 ko.applyBindings(viewModel);
 
 setInterval(function() {viewModel.totalTime(viewModel.totalTime() + 1);}, 1000);
--- design/html/js/viewmodel.js
+++ design/html/js/viewmodel.js
@@ -1,27 +1,31 @@
 ﻿function ViewModel() {
 	var self = this;
-	
 	self.today = ko.observable();
 	self.totalTime = ko.observable();
-	self.totalDays = ko.computed(function() {
-		return Math.floor(self.totalTime() / 86400);
-	});
-	self.totalHours = ko.computed(function() {
-		return Math.floor((self.totalTime() - (self.totalDays() * 86400)) / 3600 );
-	});
-	self.totalMinutes = ko.computed(function() {
-		return Math.floor((self.totalTime() - (self.totalDays() * 86400) - (self.totalHours() * 3600)) / 60 );
-	});
-	self.totalSeconds = ko.computed(function() {
-		return pad(Math.floor((self.totalTime() - (self.totalDays() * 86400) - (self.totalHours() * 3600) - (self.totalMinutes () * 60))), 2);
-	});
-	
-	
+	self.tasks = ko.observableArray();
 }
 
-function pad(num, size) {
-    var s = num+"";
-    while (s.length < size) s = "0" + s;
-    return s;
+function TimeEntry(start) {
+    var self = this;
+    self.start =  ko.observable(start);
+    self.stop =  ko.observable();
+    self.note =  ko.observable();
 }
+
+function TaskStatus(id, name) {
+    var self = this;
+    self.id = id;
+    self.name = name
+}
+
+function Task(name) {
+    var self = this;
+    self.name = name;
+    self.description = ko.observable();
+    self.dueDate = ko.observable();
+    self.tags = ko.observableArray();
+    self.status = ko.observable([new TaskStatus(1, 'New')]);
+    self.timeEntries = ko.observableArray();    
+}
+
 
